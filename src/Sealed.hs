09ba24cb3067cad29aa814bb7366706745ca2a2d
Matthias Goergens <matthias.goergens@gmail.com>
2023-04-26 12:32:23 +0800

    OK

--- src/Sealed.hs
+++ src/Sealed.hs
@@ -1,6 +1,30 @@
 module Sealed where
 
-import BatchClass qualified as BC
+import Data.List qualified as DL
 
-class Sorted s where
-    pull :: (Ord a, BC.Batch b) => s b a -> Maybe (b a, s b a)
+import BatchClass qualified as BC
+import Data.Function (on)
+-- import Data.Ord (compare)
+
+class BC.Batch b => Sorted s b where
+    -- double pull
+    pull2 :: Ord a => s b a -> Maybe (Heap s b a)
+    pull2 s = case pull1 s of
+        Nothing -> Nothing
+        Just (Heap b1 s1) -> case pull1 s1 of
+            Nothing -> Just (Heap b1 s1)
+            Just (Heap b2 s2) -> Just (Heap (b1 <> b2) s2)
+
+    pull1 :: Ord a => s b a -> Maybe (Heap s b a)
+    insert :: Ord a => Heap s b a -> s b a -> s b a
+
+data Heap s b a = Heap { batch :: b a, rest :: s b a }
+newtype SList b a = SList [Heap SList b a]
+
+instance BC.Batch b => Sorted SList b where
+    insert x (SList xs) = SList $ DL.insertBy (compare `on` BC.key . batch) x xs
+    pull1 (SList []) = Nothing
+    pull1 (SList (Heap batch0 rest:xs))
+        = Just (Heap batch0 $ maybe id insert (pull2 rest) (SList xs))
+
+-- pop :: 
