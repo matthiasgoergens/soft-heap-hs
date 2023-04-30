bc6104be4c6e63ec8527d59d5f1a3c340df6fb1b
Matthias Görgens <matthias.goergens@gmail.com>
2023-04-30 21:19:36 +0800

    ok

--- py/softheap.py
+++ py/softheap.py
@@ -7,14 +7,16 @@ from dataclasses import dataclass
 import dataclasses as d
 import math
 from math import inf
-from typing import Union, Tuple, List
+from typing import Union, Tuple, List, Iterator
 from copy import copy
-T = inf
-# T = 2
+# T = inf
+T = 3
 
 ########
 # Item #
 ########
+
+debug = False
 
 
 @d.dataclass()
@@ -25,6 +27,30 @@ class Item:
     def __post_init__(self):
         if self.next is None:
             self.next = self
+
+    def size(self):
+        cur = self
+        count = 0
+        while True:
+            if debug:
+                print(f"Item.size: {cur.key}")
+            if cur is None or cur is nullItem:
+                raise Exception("Item.size(): cur is None or cur is nullItem")
+                # return count
+            count += 1
+            cur = cur.next
+            if cur is self or cur is None or cur is nullItem:
+                if debug:
+                    print(f"Item.size: {count}")
+                return count
+
+    def items(self):
+        cur = self
+        while True:
+            yield cur.key
+            cur = cur.next
+            if cur is self or cur is None or cur is nullItem:
+                return
 
 nullItem = Item(inf)
 
@@ -58,6 +84,51 @@ class Node:
                 self.right = null
             except NameError:
                 self.left = self
+    
+    def items1(self):
+        # print("foo")
+        global debug
+        # if debug:
+        #     print(f"Node.items1 key: {self.key}")
+        yield from self.set.items()
+        if self.left is not None and self.left is not null and self.left is not self:
+            yield from self.left.items1()
+        if self.right is not None and self.right is not null and self.right is not self:
+            yield from self.right.items1()
+
+    def size1(self):
+        if debug:
+            print(f"Node.size1(): key: {self.key}")
+            items = list(self.items1())
+            print (f"size1: {self.set.size()}\t{items}")
+        count = self.set.size()
+        for child in (self.left, self.right):
+            if child is not None and child is not null and child is not self:
+                count += child.size1()
+        return count
+        # return (self.set.size() +
+        #         (0 if self.left is None or self.left is null or self.left is self else self.left.size1()) +
+        #         (0 if self.right is None or self.right is null or self.right is self else self.right.size1()))
+
+
+    def items(self):
+        cur = self
+        while True:
+            yield from cur.items1()
+            cur = cur.next
+            if cur is self or cur is None or cur is null:
+                return
+
+    def size(self):
+        if debug:
+            print(f"Node.size() ###############################")
+        cur = self
+        count = 0
+        while True:
+            count += self.size1()
+            cur = cur.next
+            if cur is self or cur is None or cur is null:
+                return count
 
 null = Node(rank=inf)
 # null.left = null
@@ -68,6 +139,17 @@ null = Node(rank=inf)
 # null.left = null
 # null.right = null
 # null.next = null
+
+def get_all_uncorrupted(P: Union[None, Node]) -> List[float]:
+    # TODO: this needs to take next into account.
+    def helper(x: Union[None, Node]) -> Iterator[float]:
+        if x is None or x is null:
+            return
+        else:
+            yield x.key
+            yield from helper(x.left)
+            yield from helper(x.right)
+    return list(helper(P))
 
 ##########
 # defill #
@@ -181,8 +263,8 @@ def reorder(H: Node, k: int) -> Node:
 ##########
 
 
-def insert(e: Item, H: Node) -> Node:
-    return key_swap(meldable_insert(make_root(e), rank_swap(H)))
+def insert(e: Union[float, int], H: Node) -> Node:
+    return key_swap(meldable_insert(make_root(Item(e)), rank_swap(H)))
 
 #############
 # make_root #
@@ -247,10 +329,25 @@ def randperm(n) -> List[float]:
     return random.sample(list(range(n)), n)
 
 
-def build(lst) -> Node:
+def build(lst: List[Union[float, int]]) -> Node:
+    global debug
+    debug = False
     P = make_heap()
+    s = 0
+    so_far = []
     for it in lst:
-        P = insert(Item(it), P)
+        s += 1
+        so_far.append(it)
+        P = insert(it, P)
+        # hmm, P.size is buggy.  Go with items.
+        assert len(so_far) == len(list(P.items())), f"{so_far} != {list(P.items()}"
+        # if P.size() != s:
+        #     msg = f"{P.size()} != {s}, {list(P.items())} {so_far}"
+        #     debug = True
+        #     # list(P.items())
+        #     # debug = False
+        #     assert P.size() == s, msg
+        #     return P
     return P
 
 
@@ -267,21 +364,36 @@ def sort(lst: List[float]) -> List[float]:
     print(lst)
     P = build(lst)
     lst1 = extract(P)
-    if T == inf:
-        for i in range(1, len(lst)):
-            if lst1[i] < lst1[i-1]:
+    corruptions = 0
+    for i in range(1, len(lst)):
+        if lst1[i] < lst1[i-1]:
+            if T == inf:
                 raise BUG("BUG!!!")
+            else:
+                corruptions += 1
+    print(f"corruptions: {corruptions}")
     print(lst1)
     print(" ")
     return lst1
 
+def sort1(lst: List[float]) -> List[float]:
+    P = build(lst)
+    print(get_all_uncorrupted(P))
+    print(P.size())
+    for _ in range(len(lst)//2):
+        P = delete_min(P)
+    print(get_all_uncorrupted(P))
+    print(P.size())
 
-sort(randperm(100))
+def from_upstream():
+    sort(randperm(100))
 
-T = 3
-sort(randperm(100))
+    T = 3
+    sort(randperm(100))
 
-# T = inf
-P = build(randperm(100))
-Q = build(randperm(200))
-print(extract(meld(P, Q)))
+    # T = inf
+    P = build(randperm(100))
+    Q = build(randperm(200))
+    print(extract(meld(P, Q)))
+
+sort1(range(100))
